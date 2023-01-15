08c56f54543e8f07b203c0d5a1586cd31dc90a69
Burtchen <Burtchen@web.de>
2023-01-15 17:44:46 +0100

    📝 add github link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,12 +8,14 @@ const { Footer: Antdfooter } = Layout;
 export const StyledFooter = styled(Antdfooter)`
   min-height: 3rem;
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: space-between;
   width: 80vw;
   max-width: 600px;
   margin-left: auto;
   margin-right: auto;
+  gap: 2rem;
 `;
 
 const Footer = () => (
@@ -21,6 +23,13 @@ const Footer = () => (
     <Link to="/">Hauptseite</Link>
     <Link to="/impressum">Impressum</Link>
     <Link to="/datenschutz">Datenschutzerklärung</Link>
+    <a
+      href="https://github.com/Burtchen/wahlkreise.berlin"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      Github
+    </a>
   </StyledFooter>
 );
 
